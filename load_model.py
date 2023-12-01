0cf148a26a49e847cfdcbb634d712356692a8859
synoce-iitgn <76461801+aman1chaudhary@users.noreply.github.com>
2023-12-01 17:47:27 +0530

    1dec

--- load_model.py
+++ load_model.py
@@ -1,3 +1,5 @@
+# When you get "thresholding_model.pkl" run this script manually using "python load_model.py" this will generate "loaded_model.pkl" that was used to predict the inundation maps
+
 import pickle
 from thresholding_model_class import ThresholdingModel
 
